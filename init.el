42ca09c751eca3b36831c10027b6334d6efb453b
Jasonᵀ <taoeta@qq.com>
2020-03-17 22:14:52 +0800

    fix

--- init.el
+++ init.el
@@ -4,14 +4,6 @@
 			 ("melpa" . "http://elpa.emacs-china.org/melpa/")))
 (package-initialize)
 
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
-
-
 ;;load path
 (defun add-subdirs-to-load-path (dir)
   "Recursive add directories to `load-path'."
@@ -19,3 +11,11 @@
     (add-to-list 'load-path dir)
     (normal-top-level-add-subdirs-to-load-path)))
 (add-subdirs-to-load-path "~/.emacs.d/elisp/")
+
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+
