9ad37d7f93526c5c6bc3e07649c7b6a1dfdb2f12
Jasonᵀ <taoeta@qq.com>
2020-03-23 22:11:05 +0800

    rainbow-delimiters

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (beacon dashboard company-restclient try magit dumb-jump ag helm-ag helm-projectile helm-projectil projectile better-shell fish-completion expand-region aggressive-indent aggresive-indent hungry-delete neotree which-key restclient restclient-helm evil org-bullets ace-window google-this youdao-dictionary helm god-mode company use-package))))
+    (rainbow-delimiters dimmer beacon dashboard company-restclient try magit dumb-jump ag helm-ag helm-projectile helm-projectil projectile better-shell fish-completion expand-region aggressive-indent aggresive-indent hungry-delete neotree which-key restclient restclient-helm evil org-bullets ace-window google-this youdao-dictionary helm god-mode company use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
