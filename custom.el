44dfa0ebca45f035f9ff26ff7e73713255a89c17
Jasonᵀ <taoeta@qq.com>
2020-04-26 22:50:04 +0800

    restart-emacs

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (ialign evil-nerd-commenter highlight-thing exec-path-from-shell vterm elpy dracula-theme spacemacs-common solarized-theme company-shell spacemacs-theme dired-k dired-icon yaml-mode company-tabnine yasnippet-snippets realgud google-c-style autodisass-java-bytecode evil-org multiple-cursors multiple-cursor hydra zzz-to-char evil-leader evil-magit evil-collection meghanada company-quickhelp rainbow-delimiters dimmer beacon dashboard company-restclient try magit dumb-jump ag helm-ag helm-projectile helm-projectil projectile better-shell fish-completion expand-region aggressive-indent aggresive-indent hungry-delete neotree which-key restclient restclient-helm evil org-bullets ace-window google-this youdao-dictionary helm god-mode company use-package))))
+    (restart-emacs ialign evil-nerd-commenter highlight-thing exec-path-from-shell vterm elpy dracula-theme spacemacs-common solarized-theme company-shell spacemacs-theme dired-k dired-icon yaml-mode company-tabnine yasnippet-snippets realgud google-c-style autodisass-java-bytecode evil-org multiple-cursors multiple-cursor hydra zzz-to-char evil-leader evil-magit evil-collection meghanada company-quickhelp rainbow-delimiters dimmer beacon dashboard company-restclient try magit dumb-jump ag helm-ag helm-projectile helm-projectil projectile better-shell fish-completion expand-region aggressive-indent aggresive-indent hungry-delete neotree which-key restclient restclient-helm evil org-bullets ace-window google-this youdao-dictionary helm god-mode company use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
