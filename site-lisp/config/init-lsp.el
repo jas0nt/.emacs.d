9616fb2e702036297159a70a72714929958219f3
Jas0nT <taoeta@gmail.com>
2024-11-23 21:55:19 +0800

    a

--- site-lisp/config/init-lsp.el
+++ site-lisp/config/init-lsp.el
@@ -6,18 +6,26 @@
 (global-lsp-bridge-mode)
 (setq lsp-bridge-nix-lsp-server "nil")
 
-;(use-package lsp-bridge
-;  :straight '(lsp-bridge :type git :host github :repo "manateelazycat/lsp-bridge"
-;            :files (:defaults "*.el" "*.py" "acm" "core" "langserver" "multiserver" "resources")
-;            :build (:not compile))
-;  :init
-;  (global-lsp-bridge-mode)
-;  (setq lsp-bridge-nix-lsp-server "nil"))
-
-
 (use-package acm-terminal
   :after (acm)
   :if (not (display-graphic-p)))
+
+
+;; (use-package lsp-bridge
+;;  :straight '(lsp-bridge :type git :host github :repo "manateelazycat/lsp-bridge"
+;;            :files (:defaults "*.el" "*.py" "acm" "core" "langserver" "multiserver" "resources")
+;;            :build (:not compile))
+;;  :init
+;;  (global-lsp-bridge-mode)
+;;  (setq lsp-bridge-nix-lsp-server "nil"))
+
+
+;; (unless (display-graphic-p)
+;;   (use-package popon
+;;    :straight '(popon :host nil :repo "https://codeberg.org/akib/emacs-popon.git"))
+;;   (use-package acm-terminal
+;;    :straight '(acm-terminal :host github :repo "twlz0ne/acm-terminal")))
+
 
 (use-package nix-mode
   :mode "\\.nix\\'")
