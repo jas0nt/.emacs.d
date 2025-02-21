bac3243791e2540144563b20edf54ce3344ad036
Jas0nT <taoeta@gmail.com>
2025-02-21 15:40:10 +0800

    update default font size and change English font for Linux

--- site-lisp/config/init-font.el
+++ site-lisp/config/init-font.el
@@ -2,14 +2,14 @@
   (cond ((eq window-system 'pgtk)
          (set-face-attribute 'default nil :height 140 :family "WenQuanYi Micro Hei Mono"))
         (t
-         (let ((emacs-font-size 15)
+         (let ((emacs-font-size 18)
                (chinese-font-name  "Source Han Sans")
                english-font-name)
            (cond
             ((featurep 'cocoa)
              (setq english-font-name "FiraCode Nerd Font"))
             ((string-equal system-type "gnu/linux")
-             (setq english-font-name "FiraCode Nerd Font")))
+             (setq english-font-name "Recursive Mono Casual Static")))
            (when (display-grayscale-p)
              (set-frame-font (format "%s-%s" (eval english-font-name) (eval emacs-font-size)))
              (set-fontset-font (frame-parameter nil 'font) 'unicode (eval english-font-name))
