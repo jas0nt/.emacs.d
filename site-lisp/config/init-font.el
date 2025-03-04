dbbd6ef638f5ab5d4cc8e7758999ba94d220661c
Jas0nT <taoeta@gmail.com>
2025-03-04 20:22:32 +0800

    simplify font setup and unify font configuration for better readability

--- site-lisp/config/init-font.el
+++ site-lisp/config/init-font.el
@@ -1,22 +1,9 @@
 (defun load-font-setup()
-  (cond ((eq window-system 'pgtk)
-         (set-face-attribute 'default nil :height 140 :family "WenQuanYi Micro Hei Mono"))
-        (t
-         (let ((emacs-font-size 18)
-               (chinese-font-name  "Source Han Sans")
-               english-font-name)
-           (cond
-            ((featurep 'cocoa)
-             (setq english-font-name "FiraCode Nerd Font"))
-            ((string-equal system-type "gnu/linux")
-             (setq english-font-name "Recursive Mono Casual Static")))
-           (when (display-grayscale-p)
-             (set-frame-font (format "%s-%s" (eval english-font-name) (eval emacs-font-size)))
-             (set-fontset-font (frame-parameter nil 'font) 'unicode (eval english-font-name))
-
-             (dolist (charset '(kana han symbol cjk-misc bopomofo))
-               (set-fontset-font (frame-parameter nil 'font) charset (font-spec :family (eval chinese-font-name))))
-             )))))
+  (set-face-attribute 'default nil :font "Recursive Mono Casual Static-18")
+  (set-fontset-font
+   t 'han
+   (font-spec :name "Source Han Sans" :size 18))
+  )
 
 (load-font-setup)
 
