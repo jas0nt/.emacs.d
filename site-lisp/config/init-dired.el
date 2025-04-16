674d4379533b413a0a59788429da47518a16d72e
Jas0nT <taoeta@gmail.com>
2025-04-16 13:05:10 +0800

    meow and dired

--- site-lisp/config/init-dired.el
+++ site-lisp/config/init-dired.el
@@ -1,6 +1,46 @@
 (use-package dirvish
+  :ensure t
+  :init
+  (dirvish-override-dired-mode)
+  :custom
+  (dirvish-quick-access-entries ; It's a custom option, `setq' won't work
+   '(("h" "~/"                          "Home")
+     ("d" "~/Downloads/"                "Downloads")
+     ("x" "/run/media/"                 "Drives")
+     ("t" "~/.local/share/Trash/files/" "TrashCan")))
   :config
-  (setq dired-listing-switches "-alht"))
+  ;; (setq dired-listing-switches "-alht")
+  (setq dired-listing-switches
+        "-l --almost-all --human-readable --group-directories-first --no-group")
+  (put 'dired-find-alternate-file 'disabled nil)
+  (dirvish-peek-mode)                ; Preview files in minibuffer
+  ;; (dirvish-side-follow-mode)      ; similar to `treemacs-follow-mode'
+  (setq dirvish-mode-line-format
+        '(:left (sort symlink) :right (omit yank index)))
+  (setq dirvish-attributes           ; The order *MATTERS* for some attributes
+        '(vc-state subtree-state nerd-icons collapse git-msg file-time file-size)
+        dirvish-side-attributes
+        '(vc-state nerd-icons collapse file-size))
+  :bind
+  (("C-x C-d" . dirvish)
+   :map dirvish-mode-map               ; Dirvish inherits `dired-mode-map'
+   ("h"   . dired-up-directory)
+   ("l"   . dired-find-file)
+   ("j"   . dired-next-line)
+   ("k"   . dired-previous-line)
+   ("/"   . dired-fd)
+   ("?"   . dirvish-dispatch)          ; [?] a helpful cheatsheet
+   ("a"   . dirvish-setup-menu)        ; [a]ttributes settings:`t' toggles mtime, `f' toggles fullframe, etc.
+   ("f"   . dirvish-file-info-menu)    ; [f]ile info
+   ("o"   . dirvish-quick-access)      ; [o]pen `dirvish-quick-access-entries'
+   ("s"   . dirvish-quicksort)         ; [s]ort flie list
+   ("r"   . dirvish-history-jump)      ; [r]ecent visited
+   ("*"   . dirvish-mark-menu)
+   ("y"   . dirvish-yank-menu)
+   ("TAB" . dirvish-subtree-toggle)
+   ("M-f" . dirvish-history-go-forward)
+   ("M-b" . dirvish-history-go-backward)
+   ("M-e" . dirvish-emerge-menu)))
 
 
 (provide 'init-dired)
